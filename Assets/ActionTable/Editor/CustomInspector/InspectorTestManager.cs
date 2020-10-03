895f28db0aa72e71cf15b8a536fe6feb13163dc1
Max <unity3damcon@gmail.com>
2020-10-03 22:28:52 +0300

    Initialization

---
+++ Assets/ActionTable/Editor/CustomInspector/InspectorTestManager.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using ActionTableSpace;
+using System;
+
+[CustomEditor(typeof(TableData))]
+public class InspectorTestManager : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        _manager = (TableData)target;
+        ShowData();
+    }
+
+    TableData _manager;
+    int countColumn;
+    int countRow;
+    int countResult;
+    Color[] colors = new Color[] { Color.white, Color.blue };
+
+    CurrentSelect currentSelect;
+
+    TypeTable currentTable = TypeTable.Table;
+
+
+    public void ShowData() {
+        countRow = Enum.GetNames(typeof(TypeObject)).Length;
+        countColumn = Enum.GetNames(typeof(TypeAction)).Length;
+        countResult = Enum.GetNames(typeof(TypeResult)).Length;
+        _manager.Initialization();
+        if (currentTable == TypeTable.Table)
+        {
+            ShowTable();
+        }
+        else
+        {
+            ShowSelect();
+        }
+
+    }
+
+    private void ShowTable() {
+        
+        
+        CreateFirstRow();
+        CreateColumns();
+    }
+
+    private void ShowSelect() {
+        GUIStyle style = GetStyle(TypeGUIStyle.Button);
+        TypeResult _tempEnum;
+        string nameButton;
+        for (int i = 0; i < countResult; i++)
+        {
+            _tempEnum = (TypeResult)i;
+            nameButton = _tempEnum.ToString();
+
+            if (GUILayout.Button(nameButton, style))
+            {
+                _manager.SetData(currentSelect.typeObject, currentSelect.typeAction, _tempEnum);
+                currentTable = TypeTable.Table;
+            }
+        }
+
+        style = GetStyle(TypeGUIStyle.BackButton);
+        
+        
+        GUILayout.Space(20);
+        if (GUILayout.Button("Back", style))
+        {
+            currentTable = TypeTable.Table;
+        }
+    }
+
+
+
+    private void CreateFirstRow() {
+        GUIStyle style = GetStyle(TypeGUIStyle.EmptyBox);
+        GUILayout.BeginHorizontal();
+        GUILayout.Box("", style);
+        style = GetStyle(TypeGUIStyle.Box);
+        TypeAction _tempEnum;
+        for (int i = 0; i < countColumn; i++)
+        {
+            _tempEnum = (TypeAction)i;
+            GUILayout.Box(_tempEnum.ToString(), style);
+        }
+        
+        GUILayout.EndHorizontal();
+    }
+
+    private void CreateColumns() {
+        TypeObject _tempEnum;
+        for (int i = 0; i < countRow; i++)
+        {
+            _tempEnum = (TypeObject)i;
+            CreateRow(_tempEnum);
+        }
+    }
+
+    private void CreateRow(TypeObject _typeObject)
+    {
+        GUIStyle style = GetStyle(TypeGUIStyle.Box);
+        GUILayout.BeginHorizontal();
+        GUILayout.Box(_typeObject.ToString(), style);
+        style = GetStyle(TypeGUIStyle.Button);
+        TypeResult _tempEnum;
+        string nameButton;
+        for (int i = 0; i < countColumn; i++)
+        {
+             _tempEnum = _manager.GetData(_typeObject, (TypeAction)i);
+            nameButton = _tempEnum.ToString();
+            
+            if (GUILayout.Button(nameButton, style))
+            {
+                currentSelect = new CurrentSelect(_typeObject, (TypeAction)i);
+                currentTable = TypeTable.Select;
+            }
+        }
+
+        GUILayout.EndHorizontal();
+    }
+
+    private GUIStyle GetStyle(TypeGUIStyle typeGUI) {
+        GUIStyle style;
+
+        switch (typeGUI)
+        {
+            case TypeGUIStyle.Button:
+                style = new GUIStyle(GUI.skin.button);
+                GUI.backgroundColor = Color.yellow;
+                style.richText = true;
+                style.fixedHeight = _manager.height;
+                style.fixedWidth = _manager.width;
+                style.normal.textColor = Color.gray;
+                style.fontStyle = FontStyle.Bold;
+                style.alignment = TextAnchor.MiddleCenter;
+                break;
+            case TypeGUIStyle.BackButton:
+                style = new GUIStyle(GUI.skin.button);
+                GUI.backgroundColor = Color.grey;
+                style.richText = true;
+                style.fixedHeight = _manager.height;
+                style.fixedWidth = _manager.width;
+                style.normal.textColor = Color.white;
+                style.fontStyle = FontStyle.Bold;
+                style.alignment = TextAnchor.MiddleCenter;
+                break;
+            case TypeGUIStyle.Box:
+                style = new GUIStyle(GUI.skin.box);
+                GUI.backgroundColor = Color.white;
+                style.richText = true;
+                style.fixedHeight = _manager.height;
+                style.fixedWidth = _manager.width;
+                style.normal.textColor = Color.blue;
+                style.fontStyle = FontStyle.BoldAndItalic;
+                style.alignment = TextAnchor.MiddleCenter;
+                break;
+            case TypeGUIStyle.EmptyBox:
+                style = new GUIStyle(GUI.skin.box);
+                GUI.backgroundColor = new Color(1, 1, 1, 0);
+                style.richText = true;
+                style.fixedHeight = _manager.height;
+                style.fixedWidth = _manager.width;
+                style.normal.textColor = Color.blue;
+                style.alignment = TextAnchor.MiddleCenter;
+                break;
+            default:
+                style = new GUIStyle(GUI.skin.box);
+                style.fixedHeight = _manager.height;
+                style.fixedWidth = _manager.width;
+                style.normal.textColor = Color.blue;
+                break;
+        }
+
+        return style;
+    }
+}
---
+++ Assets/ActionTable/Scripts/TableData.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ActionTableSpace {
+    public class TableData : MonoBehaviour
+    {
+        [Header("Size")]
+        public float height = 25;
+        public float width = 40; 
+
+        [Space]
+
+        public List<DataForObject> dataForObjects;
+
+        public TypeResult GetData(TypeObject _object, TypeAction _action) {
+            DataForObject _data = dataForObjects.Find(x => x.typeObject == _object);
+            DataWithAction _dataAction = _data.withActions.Find(x => x.typeAction == _action);
+            return _dataAction.typeResult;
+        }
+
+        public void SetData(TypeObject _object, TypeAction _action, TypeResult _result)
+        {
+            DataForObject _data = dataForObjects.Find(x => x.typeObject == _object);
+            DataWithAction _dataAction = _data.withActions.Find(x => x.typeAction == _action);
+            _dataAction.typeResult = _result;
+        }
+
+
+        public void Initialization() {
+            
+            if (dataForObjects == null || dataForObjects.Count == 0)
+            {
+                Debug.LogError(dataForObjects.Count);
+                TypeObject temp;
+                for (int i = 0; i < Enum.GetNames(typeof(TypeObject)).Length; i++)
+                {
+                    temp = (TypeObject)i;
+                    dataForObjects.Add(new DataForObject(temp));
+                }
+
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class DataForObject
+    {
+        public TypeObject typeObject;
+        public List<DataWithAction> withActions;
+
+        public DataForObject(TypeObject typeObject)
+        {
+            Initialization(typeObject);
+        }
+
+        public void Initialization(TypeObject type)
+        {
+            typeObject = type;
+            withActions = new List<DataWithAction>();
+            TypeAction temp;
+            for (int i = 0; i < Enum.GetNames(typeof(TypeAction)).Length; i++)
+            {
+                temp = (TypeAction)i;
+                withActions.Add(new DataWithAction(temp));
+            }
+        }
+
+    }
+
+    [System.Serializable]
+    public class DataWithAction{
+        public TypeAction typeAction;
+        public TypeResult typeResult;
+
+        public DataWithAction(TypeAction typeAction) {
+            this.typeAction = typeAction;
+        }
+    }
+
+    public enum TypeObject {
+        Object1,
+        Object2
+    }
+
+    public enum TypeAction
+    {
+        Action1,
+        Action2,
+        Action3
+    }
+
+    public enum TypeGUIStyle
+    {
+        Button,
+        BackButton,
+        Box,
+        EmptyBox
+    }
+
+    public enum TypeTable
+    {
+        Table,
+        Select
+    }
+
+
+
+    public enum TypeResult
+    {
+        Result1,
+        Result2,
+        Result3,
+        Result4,
+        Result5
+    }
+
+    public class CurrentSelect {
+        public TypeObject typeObject;
+        public TypeAction typeAction;
+
+        public CurrentSelect(TypeObject typeObject, TypeAction typeAction) {
+            this.typeObject = typeObject;
+            this.typeAction = typeAction;
+        }
+    }
+}
